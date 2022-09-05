8af97c8ff981b902c5fa72130f5319eed06fda41
Dillion Verma <hello@dillionverma.com>
2022-09-04 20:28:10 -0400

    ✨ feat: MVP server done

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,27 @@
 interface Config {
-  smspoolApiKey: string;
+  host: string;
+  dalleApiKey: string;
+  supabaseUrl: string;
+  supabaseApiKey: string;
+  telegramToken: string;
+  telegramUserId: string;
+  awsAccessKey: string;
+  awsSecretKey: string;
+  adminMacaroon: string;
+  tlsCert: string;
 }
 
 const config: Config = {
-  smspoolApiKey: process.env.SMS_POOL_API_KEY,
+  host: process.env.HOST,
+  dalleApiKey: process.env.DALLE_API_KEY,
+  supabaseUrl: process.env.SUPABASE_URL,
+  supabaseApiKey: process.env.SUPABASE_API_KEY,
+  telegramToken: process.env.TELEGRAM_TOKEN,
+  telegramUserId: process.env.TELEGRAM_USER_ID,
+  awsAccessKey: process.env.AWS_ACCESS_KEY,
+  awsSecretKey: process.env.AWS_SECRET_KEY,
+  adminMacaroon: process.env.ADMIN_MACAROON,
+  tlsCert: process.env.TLS_CERT,
 };
 
 export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,84 +1,221 @@
+import { createClient } from "@supabase/supabase-js";
+import aws from "aws-sdk";
 import axios from "axios";
-import express from "express";
+import cors from "cors";
+import express, { Request } from "express";
+import lnService from "lightning";
+import { Telegram } from "telegraf";
 import config from "./config";
+import { Dalle, ImageData } from "./Dalle";
+
+aws.config.credentials = new aws.Credentials(
+  config.awsAccessKey,
+  config.awsSecretKey
+);
+const s3 = new aws.S3();
+
+const telegram: Telegram = new Telegram(config.telegramToken);
+
+const supabase = createClient(config.supabaseUrl, config.supabaseApiKey);
+
+type Order = {
+  id: number;
+  created_at: Date;
+  invoice_id: string;
+  invoice_request: string;
+  generated: boolean;
+  satoshis: number;
+  results: string[];
+};
+
+const { adminMacaroon: macaroon, tlsCert: cert, host } = config;
+const socket = `${host}:10009`;
+const { lnd } = lnService.authenticatedLndGrpc({
+  macaroon,
+  socket,
+});
+
+const dalle = new Dalle({ apiKey: config.dalleApiKey });
+
 const PORT = 3001;
 const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+app.use(express.json()); // parse application/json
 
-app.get("/", (req, res) => {
+const BUCKET_NAME = "dalle2-lightning";
+
+const uploadFileToS3 = async (
+  url: string,
+  bucket: string = BUCKET_NAME,
+  key: string
+): Promise<string> => {
+  return axios
+    .get(url, { responseType: "arraybuffer", responseEncoding: "binary" })
+    .then(async (response) => {
+      const params = {
+        ContentType: "image/png",
+        ContentLength: response.data.length.toString(), // or response.header["content-length"] if available for the type of file downloaded
+        Bucket: bucket,
+        Body: response.data,
+        Key: key,
+      };
+      await s3.putObject(params).promise();
+      return `https://${BUCKET_NAME}.s3.amazonaws.com/${key}`;
+    })
+    .catch((e) => {
+      console.log(e);
+      console.log("error");
+      return "";
+    });
+};
+
+app.get("/", async (req, res) => {
+  // const img =
+  //   "https://dalle2-lightning.s3.amazonaws.com/fec41299d7bda695a47efd4b9c28bef1a27d6576e12808b8220403c72b2d23db.png";
+  // const rs = await uploadFileToS3(img, BUCKET_NAME, "test5.png");
+  // console.log(rs);
   res.status(200).send("Hello World");
 });
 
-app.get("/countries", async (req, res) => {
+app.get("/invoice", async (req, res) => {
   try {
-    const response = await axios.get(
-      "https://smspool.net/api/country/retrieve_all"
-    );
-
-    res.status(200).send(response.data);
-  } catch (e) {
-    console.log(e);
-    res.status(500).send(e);
-  }
-});
-
-app.get("/services", async (req, res) => {
-  try {
-    const response = await axios.get(
-      "https://smspool.net/api/service/retrieve_all"
-    );
-
-    res.status(200).send(response.data);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-app.get("/purchase", async (req, res) => {
-  const { country, service } = req.query;
-
-  // TODO: validate that the lightning payment has been sent! :)
-
-  if (!country) {
-    res.status(400).send({ error: "Missing country id" });
-  } else if (!service) {
-    res.status(400).send({ error: "Missing service id" });
-  }
-
-  try {
-    const response = await axios.get(
-      `https://smspool.net/api/purchase/sms?key=${config.smspoolApiKey}&country=${country}&service=${service}`
-    );
-
-    res.status(200).send(response.data);
-  } catch (e) {
-    res.status(500).send(e);
-  }
-});
-
-app.get("/check", async (req, res) => {
-  const { orderid } = req.query;
-
-  if (!orderid) {
-    res.status(400).send({ error: "Missing order id" });
-  }
-
-  try {
-    const response = await axios.get(
-      `https://smspool.net/api/sms/check?key=${config.smspoolApiKey}&orderid=${orderid}`,
+    const invoice = await lnService.createInvoice({
+      lnd,
+      description: "Dalle-2 Image generation request",
+      tokens: 1000,
+    });
+    const { data, error } = await supabase.from("Orders").insert([
       {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-      }
-    );
+        invoice_id: invoice.id,
+        invoice_request: invoice.request,
+        satoshis: invoice.tokens,
+      },
+    ]);
 
-    res.status(200).send(response.data);
+    const text = `
+    New Order request
+    Invoice ID: ${invoice.id}
+    Invoice Request: ${invoice.request}
+    Invoice Tokens: ${invoice.tokens}
+    `;
+
+    await telegram.sendMessage(config.telegramUserId, text);
+    if (error) return res.status(500).send({ error: error.message });
+    console.log("Invoice generated: ", invoice);
+    res.status(200).send(invoice);
   } catch (e) {
-    res.status(500).send(e);
+    res.status(500).send({ error: e.message });
   }
 });
+
+// const img =
+// uploadFileToS3(<your_file_url>, <your_s3_path>, <your_s3_bucket>)
+//    .then(() => console.log("File saved!"))
+//    .catch(error) => console.log(error));
+
+app.post(
+  "/generate",
+  async (
+    req: Request<
+      unknown,
+      unknown,
+      { prompt: string; invoiceId: string },
+      unknown
+    >,
+    res
+  ) => {
+    // Check if params passed
+    if (!req.body.prompt || !req.body.invoiceId) {
+      res.status(400).send({ error: "Missing parameters" });
+      return;
+    }
+    try {
+      // get invoice from lnd
+      const { prompt, invoiceId: id } = req.body;
+      const invoice = await lnService.getInvoice({
+        lnd,
+        id,
+      });
+
+      if (!invoice) return res.status(400).send({ error: "Invoice not found" });
+
+      // Get order object from database
+      const { data: order, error } = await supabase
+        .from<Order>("Orders")
+        .select("*")
+        .match({ invoice_id: invoice.id })
+        .limit(1)
+        .single();
+
+      if (invoice.is_confirmed && !order.generated) {
+        // Update order to indicate that images are being generated
+        const { data: updateOrder, error } = await supabase
+          .from<Order>("Orders")
+          .update({ generated: true })
+          .match({ invoice_id: invoice.id })
+          .single();
+
+        if (error) {
+          console.error(error);
+          return res.status(500).send({ error: error.message });
+        }
+
+        // Generate images
+        const { data } = await dalle.generate(prompt);
+        // const images = data.map((data) => data.generation.image_path);
+
+        // Upload to S3
+        const images: string[] = await Promise.all(
+          data.map((image: ImageData, index: number) =>
+            uploadFileToS3(
+              image.generation.image_path,
+              BUCKET_NAME,
+              `${id}-${index}.png`
+            )
+          )
+        );
+
+        console.log(images);
+
+        // Send telegram message to myself
+        const text = `
+        Received new order!
+        Prompt: "${prompt}"
+        Invoice ID: ${invoice.id}
+        Satoshis: ${invoice.tokens}
+        `;
+
+        await telegram.sendMediaGroup(
+          config.telegramUserId,
+          images.map((img) => ({ type: "photo", media: img, caption: prompt }))
+        );
+        await telegram.sendMessage(config.telegramUserId, text);
+
+        // Update order to indicate that images have been generated
+        const { data: updatedOrder, error: error2 } = await supabase
+          .from<Order>("Orders")
+          .update({ results: images })
+          .match({ invoice_id: invoice.id })
+          .single();
+
+        if (error2) {
+          console.error(error);
+          return res.status(500).send({ error: error2.message });
+        }
+
+        res.status(200).send(images);
+      } else if (invoice.is_canceled) {
+        res.status(400).send({ error: "Invoice cancelled" });
+      } else {
+        res.status(400).send({ error: "Invoice not paid yet" });
+      }
+    } catch (e) {
+      console.error(e);
+      res.status(500).send({ error: e.message });
+    }
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
